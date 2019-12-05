84003e7b25bbf5ab7ab47fa8184740f5e34181ed
Thomas Sherer <olc0710@yahoo.com>
2019-12-05 14:10:11 -0800

    added 3 and 4 work

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -58,11 +58,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-  var adderTwo = a + b + c;
-  var prodTwo = a * b * c;
-  var statementThree = `${a} and ${b} and ${c} sum to ${adderTwo}.`;
-  var statementFour = `The product of ${a} and ${b} and ${c} is ${prodTwo}.`;
-  var thirdArray = [adderTwo, prodTwo, statementThree, statementFour];
+  // console.log('starting values: ', a, b, c);
+  var adderTwoA = sum(a, b);
+  // console.log('adderTwoA: ', adderTwoA[0]);
+  var adderTwo = sum(adderTwoA[0], c);
+  // console.log('adderTwo: ', adderTwo[0]);
+  var prodTwoA = multiply(a, b);
+  // console.log('prodTwoA: ', prodTwoA[0]);
+  var prodTwo = multiply(prodTwoA[0], c);
+  var statementThree = `${a} and ${b} and ${c} sum to ${adderTwo[0]}.`;
+  var statementFour = `The product of ${a} and ${b} and ${c} is ${prodTwo[0]}.`;
+  var thirdArray = [adderTwo[0], prodTwo[0], statementThree, statementFour];
   return thirdArray;
 }
 
@@ -87,14 +93,23 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  var sumA = sum(sumArr[0], sumArr[1]);
+  // console.log('sumA = ', sumA);
+  var sumB = sum(sumA[0], sumArr[2]);
+  // console.log('sumB = ', sumB);
+  var summer = sumB[0];
+  // console.log('summer = ', summer);
+  var statementFive = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${summer} is their sum.`;
+  // console.log('string = ', statementFive);
+  var fourthArray = [summer, statementFive];
+  return fourthArray;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
-
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+
 
 /////////////////////////////////////
 /* Problem 5
